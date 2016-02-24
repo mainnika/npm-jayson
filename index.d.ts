4821440d369cc3fa41fb80769db2006a6f9a15a2
mainnika <nikita@tokarch.uk>
2016-02-24 23:25:08 +0500

    fix & beauty

--- index.d.ts
+++ index.d.ts
@@ -2,133 +2,139 @@ import {EventEmitter} from "events";
 import {Url} from "url";
 
 import {
-	RequestOptions as NodeHttpsRequestOptions,
-	Server as NodeHttpsServer,
-	ServerOptions as NodeHttpsServerOptions,
+RequestOptions as NodeHttpsRequestOptions,
+Server as NodeHttpsServer,
+ServerOptions as NodeHttpsServerOptions,
 } from "https";
 
 import {
-	RequestOptions as NodeHttpRequestOptions,
-	Server as NodeHttpServer,
+RequestOptions as NodeHttpRequestOptions,
+Server as NodeHttpServer,
 } from "http";
 
 import {
-	Server as NodeTcpServer,
+Server as NodeTcpServer,
 } from "net";
 
 import {
-	TlsOptions,
-	Server as NodeTlsServer,
+TlsOptions,
+Server as NodeTlsServer,
 } from "tls";
 
 interface ClientOptions {
-	reviver?:Function,
-	replacer?:Function,
-	version?:number,
-	generator?:Function,
-	encoding?:string,
+    reviver?: Function,
+    replacer?: Function,
+    version?: number,
+    generator?: Function,
+    encoding?: string,
 }
 
 interface ServerOptions {
-	reviver?:Function,
-	replacer?:Function,
-	router?:Function,
-	collect?:boolean,
-	params?:any,
-	version?:number,
-	encoding?:string,
+    reviver?: Function,
+    replacer?: Function,
+    router?: Function,
+    collect?: boolean,
+    params?: any,
+    version?: number,
+    encoding?: string,
 }
 
 interface MiddlewareOptions {
-	end:boolean,
+    end: boolean,
 }
 
 interface ServerTcpOptions {
-	allowHalfOpen?: boolean;
-	pauseOnConnect?: boolean;
+    allowHalfOpen?: boolean;
+    pauseOnConnect?: boolean;
 }
 
 interface MethodOptions {
-	handler?:Function,
-	collect?:boolean,
-	params?:any,
+    handler?: Function,
+    collect?: boolean,
+    params?: any,
 }
 
 interface IJsonRpcMessage {
-	id?:any,
-	result?:any,
-	error?:any,
-	params?:any,
-	method?:string,
-	version:number,
+    id?: any,
+    result?: any,
+    error?: any,
+    params?: any,
+    method?: string,
+    version: number,
 }
 
-export declare class Client extends EventEmitter {
-	constructor(server?: Server, options?: ClientOptions);
-	constructor(options?: ClientOptions);
-
-	request(method: string, params: any, id?: any, callback?: Function): void;
-	request(method: string, params: any, callback?: Function): void;
-	request(method: Array<any>, params: any, id?: any, callback?: Function): void;
-	request(method: Array<any>, params: any, callback?: Function): void;
-	request(method: Array<any>, callback?: Function): void;
-
-	static http(url: string): ClientHttp;
-	static http(options: ClientHttpOptions): ClientHttp;
-
-	static https(url: string): ClientHttps;
-	static https(options: ClientHttpsOptions): ClientHttps;
-
-	static tcp(url: string): ClientTcp;
-	static tcp(options: ClientOptions): ClientTcp;
-
-	static tls(url: string): ClientTls;
-	static tls(options: ClientTlsOptions): ClientTls;
+interface IJsonRpcError {
+    code: number,
+    message: any,
+    data?: any,
 }
 
-export declare class Server extends EventEmitter {
-	constructor(methods?: Methods, options?: ServerOptions);
+export class Client extends EventEmitter {
+    constructor(server?: Server, options?: ClientOptions);
+    constructor(options?: ClientOptions);
 
-	method(name: string, definition: Function);
-	method(name: string, definition: Client);
-	method(name: string, definition: Method);
+    request(method: string, params: any, id?: any, callback?: Function): void;
+    request(method: string, params: any, callback?: Function): void;
+    request(method: Array<any>, params: any, id?: any, callback?: Function): void;
+    request(method: Array<any>, params: any, callback?: Function): void;
+    request(method: Array<any>, callback?: Function): void;
 
-	methods(methods?: Object);
+    static http(url: string): ClientHttp;
+    static http(options: ClientHttpOptions): ClientHttp;
 
-	hasMethod(name: string): boolean;
+    static https(url: string): ClientHttps;
+    static https(options: ClientHttpsOptions): ClientHttps;
 
-	removeMethod(name: string);
+    static tcp(url: string): ClientTcp;
+    static tcp(options: ClientOptions): ClientTcp;
 
-	getMethod(name: string): Method;
-
-	error(code?: number, message?: string, data?: any);
-
-	call(request: Object, callback?: Function);
-	call(request: Array<any>, callback?: Function);
-	call(request: string, callback?: Function);
-
-	http(): HttpServer;
-
-	https(options?: NodeHttpsServerOptions): HttpsServer;
-
-	middleware(options?: MiddlewareOptions): MiddlewareFunction;
-
-	tcp(options?: ServerTcpOptions): TcpServer;
-
-	tls(options?: TlsOptions): TlsServer;
+    static tls(url: string): ClientTls;
+    static tls(options: ClientTlsOptions): ClientTls;
 }
 
-export declare class Method {
-	constructor(handler: Function, options?: MethodOptions);
-	constructor(options?: MethodOptions);
+export class Server extends EventEmitter {
+    constructor(methods?: Methods, options?: ServerOptions);
 
-	getHandler(): Function;
+    method(name: string, definition: Function): void;
+    method(name: string, definition: Client): void;
+    method(name: string, definition: Method): void;
 
-	execute(server: Server, params?: any, callback?: Function);
+    methods(methods?: Object): void;
+
+    hasMethod(name: string): boolean;
+
+    removeMethod(name: string): void;
+
+    getMethod(name: string): Method;
+
+    error(code?: number, message?: string, data?: any): IJsonRpcError;
+
+    call(request: Object, callback?: Function): void;
+    call(request: Array<any>, callback?: Function): void;
+    call(request: string, callback?: Function): void;
+
+    http(): HttpServer;
+
+    https(options?: NodeHttpsServerOptions): HttpsServer;
+
+    middleware(options?: MiddlewareOptions): MiddlewareFunction;
+
+    tcp(options?: ServerTcpOptions): TcpServer;
+
+    tls(options?: TlsOptions): TlsServer;
+}
+
+export class Method {
+    constructor(handler: Function, options?: MethodOptions);
+    constructor(options?: MethodOptions);
+
+    getHandler(): Function;
+
+    execute(server: Server, params?: any, callback?: Function): void;
 }
 
 interface Methods {
-	[name: string]: Function;
+    [name: string]: Function;
 }
 
 interface ClientHttpOptions extends ClientOptions, NodeHttpRequestOptions {
@@ -154,29 +160,29 @@ interface TcpServer extends NodeTcpServer {
 interface TlsServer extends NodeTlsServer {
 }
 interface MiddlewareFunction {
-	(req, res, next): void;
+    (req: any, res: any, next?: Function): void;
 }
 
 export module Utils {
-	export module JSON {
-		interface StringifyOptions {
-			replacer?: Function;
-		}
+    export module JSON {
+        interface StringifyOptions {
+            replacer?: Function;
+        }
 
-		interface StringifyCallback {
-			(err: any, str: string);
-		}
+        interface StringifyCallback {
+            (err: any, str: string): void;
+        }
 
-		interface ParseOptions {
-			reviver?:Function;
-		}
+        interface ParseOptions {
+            reviver?: Function;
+        }
 
-		interface ParseCallback {
-			(err: any, obj: Object);
-		}
+        interface ParseCallback {
+            (err: any, obj: Object): void;
+        }
 
-		export declare function stringify(obj: any, options: StringifyOptions, callback: StringifyCallback);
+        export function stringify(obj: any, options: StringifyOptions, callback: StringifyCallback): void;
 
-		export declare function parse(str: string, options: ParseOptions, callback: ParseCallback);
-	}
+        export function parse(str: string, options: ParseOptions, callback: ParseCallback): void;
+    }
 }
